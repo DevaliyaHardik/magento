5709b2a6389ee8f3fc88cee3833c999ff0ec4285
DevaliyaHardik <97013624+DevaliyaHardik@users.noreply.github.com>
2022-05-16 17:14:28 +0530

    category,product,eav module complate

--- app/code/local/Hd/Vendor/Model/Session.php
+++ app/code/local/Hd/Vendor/Model/Session.php
@@ -1,5 +1,5 @@
 <?php
-class Ccc_User_Model_Session extends Mage_Core_Model_Session_Abstract {
+class Hd_Vendor_Model_Session extends Mage_Core_Model_Session_Abstract {
 	public function __construct() {
 		$this->init('adminhtml');
 	}
